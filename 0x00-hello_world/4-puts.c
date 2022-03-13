c284197149ca0c6f9d8a22a15e4e125f6482d1fb
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-13 18:10:36 +0000

    printing stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-/* printing a c programme with puts */
 #include <stdio.h>
 int main (void)
 	{
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts('"Programming is like building a multilingual puzzle\n');
 	return 0;
 }
