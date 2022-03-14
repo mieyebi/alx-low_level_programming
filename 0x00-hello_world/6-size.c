6e98661c5c71c71d2a3b26196d1ca42c9cebb822
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-14 16:08:34 +0000

    using the sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int lonlongintType;
+	float floatType;
+
+		printf("size of a char: %lu byte(s)\n", sizeof(charType));
+		printf("size of an int: %lu byte(s)\n", sizeof(intType));
+		printf("size of a long int: %lu byte(s)\n", sizeof(longintType));
+		printf("size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
+		printf("size of a float: %lu byte(s)\n", sizeof(floatType));
+		return (0);
+)
+	
