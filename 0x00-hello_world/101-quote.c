9b1fa74c4d7b50fe875e6a3487596492549e7f94
mieyebi <wilsondiann12@gmail.com>
2022-03-14 23:44:48 +0100

    printing followed by stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
-/**
- * main- does printing without printf nor puts
- */
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main- does printing without printf nor puts
+ *
+ * Return: 1 to caller
+ */
 int main(void)
 {
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
