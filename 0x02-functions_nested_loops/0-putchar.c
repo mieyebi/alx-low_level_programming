c78d9f5f97cd3a75056db79b240d6c2a4e8763dc
mieyebi <wilsondiann12@gmail.com>
2022-03-16 11:41:13 +0100

    using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,17 +10,17 @@
 #include "main.h"
 int main(void)
 {
-int ab = 'p', ac = 'u', ad = 't', ae = 'c', af = 'h', ag = 'a', ah = 'r';
-int ai = '_';	
+	int ab = 'p', ac = 'u', ad = 't', ae = 'c', af = 'h', ag = 'a', ah = 'r';
+	int ai = '_';	
 /* write the character to a stdout*/
-	_putchar(ai);
-	_putchar(ab);
-	_putchar(ac);
-	_putchar(ad);
-	_putchar(ae);
-	_putchar(af);
-	_putchar(ag);
-	_putchar(ah);
-	_putchar('\n');
+	_putchar (ai);
+	_putchar (ab);
+	_putchar (ac);
+	_putchar (ad);
+	_putchar (ae);
+	_putchar (af);
+	_putchar (ag);
+	_putchar (ah);
+	_putchar ('\n');
 	return(0);
 }
