d2a1de0bd26e231f982044e8eb6674c011a09ae7
mieyebi <wilsondiann12@gmail.com>
2022-03-16 12:27:18 +0100

    printing alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* print_alphabet_x10 prints 10 times the lowecase alphabets
+*
+* main.h calls out the function from the header file
+* _putchar prints out the characters
+*/
+
+void print_alphabet_x10(void)
+{	int am = 'a', 10;
+	while (am <= 10)
+		{	while (am <= 'z')
+			{	_putchar(am);
+				am++;
+			}
+	_putchar('\n');
+	}
+}
