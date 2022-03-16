84f80e646d0da122b20bd3c02036fd9c96c83776
mieyebi <wilsondiann12@gmail.com>
2022-03-16 08:48:21 +0100

    using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 /**
-* putchar - prints _putchar to a stdout
+* _putchar - prints to a stdout
 *
-* 
+* main - performs the putchar function
 *
 * return: 0 on success
 *
@@ -11,14 +11,26 @@
 
 int main(void)
 
-	char a = _putc;
-	char b = har;
+	char 1 = '_';
+	char 2 = p;
+	char 3 = u;
+	char 4 = t;
+	char 5 = c;
+	char 6 = h;
+	char 7 = a;
+	char 8 = r;
 {	/* write the character to a stdout*/
 
-		_putchar(a);
-		_putcha(b);
+		_putchar(1);
+		_putchar(2);
+		_putchar(3);
+                _putchar(4);
+		_putchar(5);
+                _putchar(6);
+                _putchar(7);
+                _putchar(8);
+
 		_putchar("/n")
 		
 		return(0);
 }
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int _putchar(char a, char b)
-{
+int _putchar(char 1, char 2, char 3, char 4, char 5, char 6, char 7, char 8)
+
+{	char 1='_';
+	char 2=p;
+	char 3=u;
+	char 4=t;
+	char 5=c;
+	char 6=h;
+	char 7=a;
+	char 8=r;
 
 	return(0);
 }
