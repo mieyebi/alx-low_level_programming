fab479795e29224dc9e94a66031bee5f6f8963c5
mieyebi <wilsondiann12@gmail.com>
2022-03-16 12:38:46 +0100

    printing alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,21 @@
 *
 * main.h calls out the function from the header file
 * _putchar prints out the characters
-*/
+* return void on success*/
 
 void print_alphabet_x10(void)
-	int am = 'a', 10;
-	while (am <= 10)
-		{	while (am <= 'z')
-			{	_putchar(am);
-				am++;
+{
+int az = 0;
+	while (az <= 10);
+	{
+	int am = 'a';
+	while (am <= 'z')
+		{
+		_putchar(am);
+		am++;
 			}
 	_putchar('\n');
+	az++;
 	}
+}
 
