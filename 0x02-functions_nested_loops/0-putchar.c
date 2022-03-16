b18e223342e8af550f1b7eefaae2cb841272115c
mieyebi <wilsondiann12@gmail.com>
2022-03-16 07:54:24 +0100

    using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**
+* putchar - prints _putchar to a stdout
+*
+* a = _putchar
+*
+* return: 0 on success
+*
+*/
+#include <stdio.h>
+
+int main(void)
+
+	void putchar (int char)
+{		char a = '_putchar;'
+/* write the character to a stdout*/
+
+		putchar(a);
+		putchar("/n")
+		
+		return(0);
+}
+
