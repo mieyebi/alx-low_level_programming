6945cd2930ef51e6b079f7e2fea9d2fdf0c909c4
mieyebi <wilsondiann12@gmail.com>
2022-03-17 10:49:51 +0100

    checking lowercases

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,13 +12,19 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 	{
 		if (_islower(c))
-	{
-		return (1);
-	}
+		{
+			return (1);
+		}
 		else
 		{
-		return (0);
+			return (0);
 		}
+	return (1);
 	}
+	else
+	{
+	return (0);
+	}
+	
 }
 
