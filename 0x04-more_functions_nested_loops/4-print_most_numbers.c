b148eb9a34aba53108bf0abbb6d5c9ab9df465e2
mieyebi <wilsondiann12@gmail.com>
2022-03-18 12:16:28 +0100

    printing numbers 0-9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print most numbers from 0-9
+*
+*/
+
+void print_most_numbers(void)
+{
+	int c;
+	c = '0';
+
+	for ((c <= '9') && !(c = '2' || c = '4'))
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void print_most_numbers(void);
 void print_numbers(void);
 int _putchar(int);
 int _isupper(int c);
