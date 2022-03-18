2bcf742b79f79e350d7115c8a0dcd8452dbbb07e
mieyebi <wilsondiann12@gmail.com>
2022-03-18 13:09:32 +0100

    printing most numbers from 0-9

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,17 +11,15 @@ void more_numbers(void)
 {
 	int a;
 	int b;
-	a = '0';
-	b = '0';
 
-	for (b <= '9')
-	{	while (a <= '14')
-		{	
+
+	for (b = '0'; b <= '9'; b++)
+	{	for (a = '0'; a <= '14'; a++)
+		{
 			_putchar(a);
-			a++;
 		}
+
 		_putchar(\'n');
-		b++;
 	}
 	_putchar(\'n');
 }
