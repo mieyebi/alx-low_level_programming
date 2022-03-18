ebe80267e8b4d1dd529a36af4acd8d316d0de53f
mieyebi <wilsondiann12@gmail.com>
2022-03-18 13:02:50 +0100

    printing most numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - prints numbers severally
+* _putchar - prints numbers
+*
+*/
+
+void more_numbers(void);
+{
+	int a;
+	int b;
+	a = '0';
+	b = '0';
+
+	while (b <= '9')
+	{	while (a <= '14')
+		{	
+			_putchar(a);
+			a++;
+		}
+		_putchar(\'n');
+		b++;
+	}
+	_putchar(\'n');
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
 int _putchar(int);
+int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
