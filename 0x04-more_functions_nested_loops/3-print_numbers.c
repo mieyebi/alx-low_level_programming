a7550651748bfb6c2de85e2e3951304aa9d2c557
mieyebi <wilsondiann12@gmail.com>
2022-03-18 08:41:31 +0100

    printing numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers
+* Return: 0
+*/
+
+void print_numbers(void)
+{	int a;
+	a = 0;
+	while (a >= 9)
+	{	_putchar(a)
+		a++;
+		_putchar("\n")
+	}
+	return (0);
+}
