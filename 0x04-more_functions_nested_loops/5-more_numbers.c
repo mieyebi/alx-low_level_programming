f6de98062b3c9d550e6851ce62808361b91bd9ad
mieyebi <wilsondiann12@gmail.com>
2022-03-18 13:14:22 +0100

    printing more numbers severally

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (b = '0'; b <= '9'; b++)
-	{	for (a = '0'; a <= '9'; a++)
+	for (b = 0; b <= 9; b++)
+	{	for (a = 0; a <= 14; a++)
 		{
 			_putchar(a);
 		}
