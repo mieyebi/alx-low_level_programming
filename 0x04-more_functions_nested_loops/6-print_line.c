4d6089c97c9c94876d0fb69957179cbcb4e012af
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-20 08:00:55 +0000

    printing lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,18 +10,17 @@ void print_line(int n)
 	int a;
         a = 0;
         if (n > 0)
-        {
-                while (a <= n; a++;)
-                {
-                        _putchar('_');
-                }
-        }
-        else
-        {
-                if (n <= 0);
-                {
-			_putchar('\n');
+	{
+		int a;
+		a = 0;
+		while (a <= n; a++;)
+		{
+			_putchar('_');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
