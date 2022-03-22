cec6426f87127932d94da242e0072d69c9454fbf
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-22 06:00:45 +0000

    printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: a string of characters
+*/
+void _puts(char *str)
+{
+	int n;
+
+	*str = n;
+
+	while (n != '\0')
+	{
+		_puts("*str\n");
+	}
+}
