faf36aafc09af01cd202a3a78ee4dc5626dce363
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-22 13:30:45 +0000

    printing every other 2 characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+* _puts - prints a string
+* @str: a string of characters
+*/
+void _puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+		a++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void puts2(char *str);
 void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
