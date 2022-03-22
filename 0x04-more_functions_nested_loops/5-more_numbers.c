c1a2b848c494adf970c3f939fb2b0e3ac02f663e
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-22 06:36:04 +0000

    printing more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (b = '0'; b <= '9'; b++)
+	for (b = 0; b <= 9; b++)
 	{
-		for (a = '0'; a < 15; a++)
+		for (a = 0; a < 15; a++)
 		{
 			if ((a / 10) == 1)
 			{
