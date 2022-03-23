ca22e00b9f02a254fbee746f1b818432894e0446
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-23 18:50:57 +0000

    printing the 9X table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 10; b++)
 		{
 			c = (a * b);
 
@@ -18,14 +18,14 @@ void times_table(void)
 			{
 				_putchar(c + '0');
 			}
-			if ((c <= 9) && (b != 0))
+			if ((c < 10) && (b != 0))
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(c + 0);
+				_putchar(c + '0');
 			}
-			else if (c > 9)
+			else if (c >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
