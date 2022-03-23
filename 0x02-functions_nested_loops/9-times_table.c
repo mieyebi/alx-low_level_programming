9bc41c0688e2989373d959892dc07320579f6129
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-23 11:19:52 +0000

    printing the 9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,24 @@
 #include "main.h"
+
+/**
+*times_table - prints the 9 times table
+*
+*/
 void times_table(void)
 {
 	int i, j;
-	
-	i = 0;
 
-	while (i <= )
+	while (j >= 0)
+	{
+		while (i >= 0)
+		{
+			_putchar((9 * i));
+			_putchar(',');
+			_putchar(' ');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
 }
+
