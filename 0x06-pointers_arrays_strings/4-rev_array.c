e691a895a8f935c09fce1c120a442005fcd31688
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-25 03:01:22 +0000

    reversing strngs

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,14 +6,15 @@
 * reverse_array : reverses the content of an array
 * @a: pointer
 * @n: number of elements of the array
-* Return
+*
 */
+
 void reverse_array(int *a, int n)
 {
 	n = sizeof(a);
 
 	while ((n >= 0) && (a[n] != '\0'))
 	{
-		a--;
+		n--;
 	}
 }
