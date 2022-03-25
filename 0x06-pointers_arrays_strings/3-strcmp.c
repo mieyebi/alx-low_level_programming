a883888f437f60eb539f41687af5672a93eeeaf0
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-25 02:46:35 +0000

    comparing strngs

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,11 +6,11 @@
 * _strcmp - compares two strings
 * @s1: string 1
 * @s2: string 2
-* Return: 0 on success
+* Return: s1 on success
 */
 
 int _strcmp(char *s1, char *s2)
 {
 	strcmp(s1, s2);
-	return (0);
+	return (s1);
 }
