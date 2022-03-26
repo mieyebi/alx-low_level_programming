734fbcca9a2710bacd41418c50cddfda6a26010e
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-26 13:50:56 +0000

    concatenating two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i, j;
+
+	i = j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while ( j <= n)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
 	return (dest);
 }
