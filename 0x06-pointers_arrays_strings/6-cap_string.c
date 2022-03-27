fb9df8e1026bf1b87522ff5c883597e77cc55e44
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-27 15:49:50 +0000

    capitalizing words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,17 @@ char *cap_string(char *c)
 {
 	int a = 0;
 
-	while (*(c + a) != '\0')
+	if (*(c + a) >= 'a' && *(c + a) <= 'z')
 	{
+		*(c + a) = *(c + a) - 32;
+	}
+	while (*(c + a) != '\0' && *(c + a) == ' ')
+        {
+                a++;
 		if (*(c + a) >= 'a' && *(c + a) <= 'z')
-		{
+                {
 			*(c + a) = *(c + a) - 32;
 		}
-		a++;
 	}
 	return (c);
 }
