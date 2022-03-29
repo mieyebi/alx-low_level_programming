a8440155a90e6fb58786a52afdaa11e359692b79
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-29 11:16:08 +0000

    returning prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* _strspn - gets the length of a prefix substring
+*
+* @s: initial string
+*
+* @accept: prefix string
+*
+* Return: 0 on success
+*
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = 0;
+
+	while (*(accept + a) != '\0')
+	{
+		*(s + a) = *(accept + a);
+		a++;
+		return (*(s + a));
+	}
+	return (0);
+}
