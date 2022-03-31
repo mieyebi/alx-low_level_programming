d2c5a805ef3bc982592122656a67806549ca6b39
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-03-31 08:26:49 +0000

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,17 +11,17 @@
 void _puts_recursion(char *s)
 {
 
-	 _putchar(*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+	_putchar(*s);
 	 s++;
 
 	if (*s != '\0')
 	{
 		_puts_recursion(s);
-	}
-
-	else if (*s == '\0')
-	{
-		 _putchar('\n');
 	}
 }
 
