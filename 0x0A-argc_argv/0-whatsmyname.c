d1bd6b14f3a4db474bccf252def3e1ea66aaaeb7
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-02 19:17:04 +0000

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#includee <stdio.h>
+
+/**
+* main - is the function
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: 0 on success
+*/
+
+int main (int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	_putchar('\n');
+	return (0);
+}
