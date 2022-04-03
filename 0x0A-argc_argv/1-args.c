c58acadd99f95fc16d8266b8a4aa87020553f253
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-03 14:13:02 +0000

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - a function
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: 0 on success
+*
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
