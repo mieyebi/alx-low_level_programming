ba296e453f8dc48b85e51f570e5af565438afcdf
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-03 19:54:27 +0000

    adding positive integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - function call to add positive integers
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: 0 on success, else 1
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (a = 0; a < argc; a++)
+	{
+		if (*argv[a] <= 0 && (*argv[a] >= 'A' && *argv[a] <= 'z'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			int sum = 0;
+
+			sum += atoi(argv[a]);
+			printf("%d\n", sum);
+		}
+	}
+	return (0);
+}
