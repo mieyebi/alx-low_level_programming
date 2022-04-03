30a586e9eb393e890f9cf1f6431cde28d754e5c8
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-03 16:32:17 +0000

    prints all arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - function call
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: 0 on success
+*
+*/
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", mul);
+		return (0);
+	}
+	else if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
