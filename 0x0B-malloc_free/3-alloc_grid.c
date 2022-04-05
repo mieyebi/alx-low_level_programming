6704e532a414530cc28a5625104ff6b3835636bf
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-05 18:05:35 +0000

    allocating grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* **alloc_grid - function call to return pointer to 2 dimensional array of integers
+*
+* @width: 1st array dimension
+*
+* @height: second array dimension
+*
+* Return: NULL or a pointer
+*
+*/
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int *w;
+	int *h;
+
+	int x, y;
+
+	x = y = 0;
+
+	if (width <= '0')
+	return (NULL);
+
+	if (height <= '0')
+	return (NULL);
+
+	a = malloc((width * height) * sizeof(int));
+
+	if (a == 0)
+	return (NULL);
+
+	while (x < width)
+		x++;
+
+	while (y < height)
+		y++;
+
+	w = malloc(x * sizeof(int));
+	h = malloc(y * sizeof(int));
+
+	while (w[x] != '\0')
+	{
+		w[x] = '0';
+		x++;
+		while (h[y] != '\0')
+		{
+			a[x][y] = '0';
+			y++;
+		}
+	}
+	return (a);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
