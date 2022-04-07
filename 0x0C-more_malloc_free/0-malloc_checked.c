be5c39284a84831cd853d5ac71dba7ed6defcb61
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-07 07:42:38 +0000

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+* *malloc_checked - allocates memory and checks process termination
+*
+* @b: unsigned int
+*
+* Return: 98 or c
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *c = (unsigned int *) malloc(sizeof(*c) * b);
+
+	if (c == 0)
+	{
+		free(c);
+		return ((unsigned int *)98);
+	}
+	return (c);
+}
