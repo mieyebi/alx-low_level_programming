1552f7233fbfb73a52fac65f226e0519f656b693
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-08 17:41:37 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* *_calloc - allocates memory for an array
+*
+* @nmemb: an unsigned integer
+*
+* @size: an unsigned integer
+*
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *cal;
+
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	cal = malloc(sizeof(unsigned int) * size);
+
+	if (cal == 0)
+		return (NULL);
+
+	for (a = 0; a < size; a++)
+	{
+		cal[a] = '0';
+	}
+	return (cal);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
 
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
