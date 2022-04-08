01b04e1097ecfd1c76bc0815a2ed37a98f97aa33
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-08 20:21:44 +0000

    array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *array_range - crestes an array of integers
+*
+* @min: an integer
+*
+* @max: an integer
+*
+* Return: NULL or pointer to array
+*
+*/
+int *array_range(int min, int max)
+{
+	int *p, a, b;
+
+	a = 0;
+
+	b = 0;
+
+	if (min > max)
+		return (NULL);
+	while (a >= min && a <= max)
+		a++;
+	p = malloc((a + 1) * sizeof(int));
+
+	if (p == 0)
+		return (NULL);
+	while (min < max)
+	{
+		p[b] = min;
+		b++;
+		min++;
+	}
+	return (p);
+}
