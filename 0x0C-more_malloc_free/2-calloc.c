a7ba792d7862286f42c776536f3f3e4b5eedf413
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-10 06:02:03 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	cal = malloc(size * nmemb);
+	else
+		cal = malloc(size * nmemb);
 
 	if (cal == 0)
 	{
