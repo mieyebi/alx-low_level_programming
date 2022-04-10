ffec696d1a13fb3d5cd2b318753913e725cbb8ce
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-10 14:39:54 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -53,7 +53,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == 0)
 	{
-		return (malloc(new_size));
+		return (new_ptr);
 	}
 	free(ptr);
 	return (new_ptr);
