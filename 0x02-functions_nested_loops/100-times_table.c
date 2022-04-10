cb4bc926f99eeba66f93275393e8331663ffbd94
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-10 17:50:40 +0000

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -44,6 +44,21 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 				}
+
+				if (b == n && c > 9 && c < 1000)
+                                {
+					_putchar((c / 100) + '0');
+					_putchar((c / 10) % 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+				else if (b != n && c > 9 && c < 1000)
+				{
+					_putchar((c / 100) + '0');
+					_putchar((c / 10) % 10 + '0');
+					_putchar(c % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			_putchar('\n');
 		}
