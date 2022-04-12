705c08abedf5506b90878e8c42775f73931e8a60
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-12 01:54:16 +0000

    define FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the name of a file
+*
+* Return: 0
+*/
+#define __FILE__
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
