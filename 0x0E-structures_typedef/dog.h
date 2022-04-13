d3a45e60e30a39133d530142373308490d284d72
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-13 19:27:38 +0000

    dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: name
  * @age: age
  * @owner:  owner
- * Description: This struct is for dog
+ * This struct is for dog
  */
 
 struct dog
@@ -16,10 +16,4 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /*DOG_H*/
+#endif
