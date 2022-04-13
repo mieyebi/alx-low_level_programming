6b51b9dc6fc3f514fc8ecf364213b56d507e4b93
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-13 20:55:02 +0000

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}
+typedef dog_t;
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
