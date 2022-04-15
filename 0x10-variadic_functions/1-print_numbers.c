ac021a6710f57fd72e8392feeb5f754b495bf62c
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-15 20:39:14 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(num, n);
 
-	for (a = 0; a < n; a++)
+	for (a = 0; a < n && n > 0; a++)
 	{
 		if (separator != 0)
 			printf("%d%s", va_arg(num, unsigned int), separator);
