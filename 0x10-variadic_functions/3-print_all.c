a1c48fd20d528be83e475926f4f7ebd542239a85
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-15 22:39:02 +0000

    function pointers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_all - prints anything
+*
+* @format: list of arguments
+*
+*/
+void print_all(const char * const format, ...)
+{
+	
