c4557ac8fc991bed2c4ac5295e8a4610b8d2ca1c
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-15 13:47:00 +0000

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == 0 || cmp == 0)
-		return (-1);
+		return (0);
 	for (a = 0; a < size; a++)
 	{
 		if (!cmp(array[a]))
