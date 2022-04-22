f3860b13537068c0e7b00d0346c7bb155a86c246
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-22 04:30:15 +0000

    add new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* *add_node_end - adds a new node at the end of a list
+*
+* @head: the first node
+*
+* @str: string of elements
+*
+* Return: NULL or new element address
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n_node;
+	size_t a;
+
+	n_node = malloc(sizeof(list_t));
+
+	if (n_node == 0)
+		return (NULL);
+
+	n_node->str = strdup(str);
+	n_node->len = strlen(str);
+	n_node->next = *head;
+	*head = n_node;
+	return (n_nose);
+}
