fc64f73fed174c9d2229657e9ab06dc6082f1d28
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-22 04:17:11 +0000

    return list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,3 +11,18 @@
 * Return: a
 *
 */
+
+size_t list_len(const list_t *h)
+{
+	size_t a = 1;
+
+	while (h != 0)
+	{
+		if (h->str == 0)
+			return (0);
+		else
+		h = h->next;
+		a++;
+	}
+	return(a);
+}
