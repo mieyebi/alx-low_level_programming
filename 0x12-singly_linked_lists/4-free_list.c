81f7c165867bcc18b9825519a3c3ed0a9e8b279c
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-22 04:51:41 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - frees a list
+*
+* @head: address allocated to the list
+*
+*/
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		head = head->next;
+		free(head->str);
+		free(head);
+	}
+}
