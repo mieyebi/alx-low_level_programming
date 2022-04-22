93cda6645d389274239e099e69df1a249275e8e7
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-22 03:57:10 +0000

    prints list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* list_len - returns the number of elements in a linked list
+*
+* @h: string of elements
+*
+* Return: a
+*
+*/
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,12 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct list
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
 {
 	char *str;
-	struct list *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
-size_t print_list(const list_t *h);
 
 #endif
