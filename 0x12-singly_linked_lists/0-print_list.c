e212ff4398e8877b92b7eefedea700fea99237e6
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-22 04:06:12 +0000

    prints list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -26,7 +26,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("%s", h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		a++;
 	}
