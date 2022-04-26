3cba8e2c80c734d7ba76f286a1380699bec00fdf
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-26 16:07:44 +0000

    get nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (head != NULL && a <= index)
+	while (head != NULL && a < index)
 	{
 		nth_node = head->next;
 		a++;
