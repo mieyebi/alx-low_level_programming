0349218b44cfb8cdf76d02fd8c6ec7982439a180
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-26 13:01:09 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* free_listint - frees a listin_t list
+*
+* @head: dynamically allocated address of list
+*
+*/
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		free(head);
+
+	while (head->next)
+	{
+		free(head->next);
+	}
+}
