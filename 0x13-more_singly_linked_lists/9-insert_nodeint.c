c8743d448be7ea2630399f8f34cdb4df3483725a
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-26 18:20:02 +0000

    insert new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = NULL;
 		return (new_node);
 	}
+	if (new_node == NULL)
+		return (NULL);
+
 	prev_node = *head;
 
 	while (*head != NULL && a < idx)
