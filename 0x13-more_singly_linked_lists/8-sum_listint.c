e6f5555c315d32fe83fb5bf74eecf1ff1466c15a
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-26 16:27:24 +0000

    sum elements of linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* sum_listint - sums all data of the linked list
+*
+* *head: head element of linked list
+*
+* Return: sum
+*
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
