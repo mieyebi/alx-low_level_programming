abaff79df567a6f16533a41be44c09afd2b1d5b9
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-26 11:38:05 +0000

    return length of listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* listint_len - returns the number of elements in a linked list
+*
+* @h: head of the list
+*
+* Return: n
+*
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t a = 0;
+
+	if (h == 0)
+		return (0);
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+		{	h = h->next;
+			a++;
+		}
+	}
+	return (a);
+}
