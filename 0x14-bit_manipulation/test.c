dc38fd285eda8c728e187f89e872433b97393367
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-28 20:19:52 +0000

    binary to unsigned integers

--- 0x14-bit_manipulation/test.c
+++ 0x14-bit_manipulation/test.c
@@ -19,8 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	else
-	while (b[a] != '\0')
+	while (b != NULL && b[a] != '\0')
 		a++;
 
 	while (a >= 0)
@@ -38,4 +37,20 @@ unsigned int binary_to_uint(const char *b)
 		mul2 = mul2 * 2;
 	}
 	return (sum);
+}
+int main(void)
+{
+    unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    return (0);
 }
