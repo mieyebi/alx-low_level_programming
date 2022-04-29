eff950888d0c48fcb552057cfb53a34ffd922321
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-04-29 23:18:36 +0000

    set binary

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* set_bit - sets that value of a bit
+*
+* @n: string of characters
+*
+* @index: index
+*
+* Return 1 or -1
+*
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int a;
+
+	if (index > 32)
+		return (-1);
+
+	a = 1 << index;
+	*n = *n | a;
+	return (1);
+}
