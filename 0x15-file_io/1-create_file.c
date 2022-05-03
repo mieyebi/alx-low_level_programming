43a427f0327ae6aec22afc641aa708a59e1f10bc
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-05-03 09:13:41 +0000

    creating files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	wr = write(fd, text_content, (len + 1));
+	wr = write(fd, text_content, (len - 1));
+
+	text_content[len] = '\0';
 
 	if (wr == -1)
 		return (-1);
