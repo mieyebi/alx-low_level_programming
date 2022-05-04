139c8521be48e54390c5b47e8f8a7d56c8888e64
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-05-04 23:00:18 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,7 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	read(file, buf, letters);
-	var = write(STDERR_FILENO, buf, strlen(buf));
+	var = write(STDIN_FILENO, buf, strlen(buf));
 
 	close(file);
 
