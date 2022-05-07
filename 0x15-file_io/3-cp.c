375f7c0ce4f70a5ffa65529c25dcd2511f8151b8
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-05-07 16:56:07 +0000

    cp file_from file_to

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+* main - copies a text file
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int cl, rd, file_to, file_from;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	buf = malloc(1024);
+
+	file_from = open(argv[1], O_RDWR);
+
+	rd = read(file_from, buf, 1024);
+
+
+	if (!file_from || rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	wr = write(file_to, buf, 1024);
+
+	if (file_to == -1 || wr == -1)
+	{
+		dprintf(STDOUT_FILENO, "Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	free(buf);
+	cl = close(file_to);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
