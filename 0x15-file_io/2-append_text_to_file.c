bd43b8e14cf8ca2cea9d4cce56dfe357d8212d27
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-05-07 13:25:00 +0000

    appending files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = "";
+		return(1);
 
 	wr = write(fd, text_content, len);
 
