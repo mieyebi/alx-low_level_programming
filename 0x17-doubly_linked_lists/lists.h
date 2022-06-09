2d220cd1ce9a377d106ac4dea1373993615f5ff5
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-09 22:19:11 +0000

    dlistint

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,19 +1,25 @@
-#include "lists.h"
-
+#ifndef LISTS_H
+#define LISTS_H
 /**
-* print_dlistint - prints elements of a doubly linked list
-* @h: header value of the list
-* Return: number of nodes
-*
-*/
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h)
-
-	size_t num = 0
-	while (h)
-		{
-			printf("%d\n, h->n);
-			h = h->next;
-			num++;
-		}
-	return (num);
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+#endif
