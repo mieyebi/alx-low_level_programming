fa78d0df88654d5fa348f3c359749d2af2f0ff1d
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-10 14:58:58 +0000

    get sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - sums all data in a list
+* @head: head of the list
+* Return: 0
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	if (head)
+	{
+		while (head->next != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
