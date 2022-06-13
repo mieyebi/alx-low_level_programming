54d5908f3e42c7e59dc60bcdc823b4cabda9dc62
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-13 22:13:15 +0000

    operations

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t num = 0;
+        size_t num = 0;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		num++;
-	}
-	return (num);
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                num++;
+        }
+        return (num);
 }
